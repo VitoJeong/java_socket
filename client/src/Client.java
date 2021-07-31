517dc748c6b9fff02c433549123b52db370328ff
vito <ckdghk987@gmail.com>
2021-07-31 16:10:50 +0900

    update: fail keeping connect

--- client/src/Client.java
+++ client/src/Client.java
@@ -5,19 +5,24 @@ import java.net.Socket;
 
 public class Client {
     public static void main(String[] args) {
+        // 서버와 통신하기 위한 socket
         Socket socket = null;
 
+        // 서버로부터 데이터를 읽어들이기 위한 입력스트림
         BufferedReader reader = null;
 
+        // 키보드(사용자)로부터 읽어들이기 위한 입력스트림
         BufferedReader input = null;
 
+        // 서버로 내보내기 위한 입력스트림
         PrintWriter writer = null;
 
         InetAddress inetAddress = null;
 
         try {
-            inetAddress = InetAddress.getByName("서버 주소 입력");
+            inetAddress = InetAddress.getByName("localhost");
 
+            // 서버로 접속
             socket = new Socket(inetAddress, 12000);
 
             reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
@@ -32,15 +37,22 @@ public class Client {
         }
 
         try {
-            String data = input.readLine();
+            while (true) {
+                // 키보드로부터 입력(서버로 보낼 메세지)
+                String data = input.readLine();
 
-            writer.println(data);
+                // 서버로 데이터 전송
+                writer.println(data);
 
-            writer.flush();
+                writer.flush();
 
-            String str = reader.readLine();
+                // 서버로부터 되돌아오는 데이터 읽어들임
+                String str = reader.readLine();
 
-            System.out.println("서버로부터 되돌아온 메세지: " + str);
+                System.out.println("서버로부터 되돌아온 메세지: " + str);
+
+                if(!socket.isClosed()) break;
+            }
 
             socket.close();
         } catch (IOException e) {
--- src/Server.java
+++ src/Server.java
@@ -4,15 +4,20 @@ import java.net.Socket;
 
 public class Server {
     public static void main(String[] args) {
-        Socket socket = null; // Client와 통신하기 위한 socket
+        // 클라이언트와 통신하기 위한 socket
+        Socket socket = null;
 
+        // 서버 생성을 위한 serverSocket
         ServerSocket serverSocket = null;
 
+        // 클라이언트로부터 데이터를 읽어들이기 위한 입력스트림
         BufferedReader reader = null;
 
+        // 클라이언트에게 데이터를 내보내기 위한 출력 스트림
         PrintWriter writer = null;
 
         try {
+            // 서버소켓 생성 - 포트바인딩
             serverSocket = new ServerSocket(12000);
         } catch (IOException e) {
             System.out.println("해당 포트가 열려있습니다!");
@@ -21,21 +26,34 @@ public class Server {
 
         try {
             System.out.println("서버 오픈!");
-            socket = serverSocket.accept();
+            while (true) {
+                // 서버 생성, 클라이언트 접속 대기(접속시 소켓 반환)
+                socket = serverSocket.accept();
 
-            reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                System.out.println("연결된 사용자: " + socket.getRemoteSocketAddress());
 
-            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())));
+                // 입력스트림 생성
+                reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-            String str = null;
+                // 출력스트림 생성
+                writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())));
 
-            str = reader.readLine();
+                // 클라이언트로부터 데이터를 읽어옴
+                String str = reader.readLine();
 
-            System.out.println("client로 부터 온 메세지: " + str);
+                System.out.println("클라이언트로 부터 온 메세지: " + str);
 
-            writer.write(str);
-            writer.flush();
+                writer.write(str);
+                writer.flush();
+
+                if (str.equals("stop") || str.isEmpty()) break;
+            }
+
+            // 연결 종료
             socket.close();
+
+            // 서버 중지
+            serverSocket.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
