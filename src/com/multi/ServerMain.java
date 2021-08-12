b8d7f14f875d20996d1727108ee4fc84e63b0ad5
vito <ckdghk987@gmail.com>
2021-08-12 23:59:39 +0900

    implement:  Broadcasting Online Status

--- src/com/multi/ServerMain.java
+++ src/com/multi/ServerMain.java
@@ -1,28 +1,11 @@
 package com.multi;
 
-import java.net.ServerSocket;
-import java.net.Socket;
-
 public class ServerMain {
     private static final int PORT = 8818;
 
     public static void main(String[] args) {
-
-        try {
-            ServerSocket serverSocket = new ServerSocket(PORT);
-            while (true) {
-                System.out.println("About to accept client connection...");
-                Socket clientSocket = serverSocket.accept();
-                System.out.println("Accepted connection from " + clientSocket);
-                ServerWorker worker = new ServerWorker(clientSocket);
-                worker.start();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-
+        Server server = new Server(PORT);
+        server.start();
     }
-
 
 }
--- src/com/multi/ServerWorker.java
+++ src/com/multi/ServerWorker.java
@@ -5,12 +5,17 @@ import org.apache.commons.lang3.StringUtils;
 import java.io.*;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 public class ServerWorker extends Thread {
 
     private final Socket clientSocket;
+    private final Server server;
+    private String login = null;
+    private OutputStream outputStream;
 
-    public ServerWorker(Socket clientSocket) {
+    public ServerWorker(Server server, Socket clientSocket) {
+        this.server = server;
         this.clientSocket = clientSocket;
     }
 
@@ -25,9 +30,9 @@ public class ServerWorker extends Thread {
         }
     }
 
-    private static void handleClientSocket(Socket clientSocket) throws IOException, InterruptedException {
+    private void handleClientSocket(Socket clientSocket) throws IOException, InterruptedException {
         InputStream inputStream = clientSocket.getInputStream();
-        OutputStream outputStream = clientSocket.getOutputStream();
+        this.outputStream = clientSocket.getOutputStream();
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
         String line;
@@ -35,10 +40,12 @@ public class ServerWorker extends Thread {
             String[] tokens = StringUtils.split(line);
             if (tokens != null && tokens.length > 0) {
                 String cmd = tokens[0];
-                if ("quit".equalsIgnoreCase(line)) {
+                if ("quit".equalsIgnoreCase(cmd)) {
                     break;
+                } else if ("login".equalsIgnoreCase(cmd)) {
+                    if (!isLoginSuccess(outputStream, tokens)) break;
                 } else {
-                    String msg = "unknown: " + cmd + "\n";
+                    String msg = login + ": " + cmd + "\n";
                     outputStream.write(msg.getBytes(StandardCharsets.UTF_8));
                 }
             }
@@ -46,5 +53,59 @@ public class ServerWorker extends Thread {
             // outputStream.write(msg.getBytes(StandardCharsets.UTF_8));
         }
         clientSocket.close();
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    private boolean isLoginSuccess(OutputStream outputStream, String[] tokens) throws IOException {
+        if (tokens.length == 3) {
+            String login = tokens[1];
+            String password = tokens[2];
+
+            if (checkAccess(login, password)) {
+                String msg = "ok login\n";
+                outputStream.write(msg.getBytes(StandardCharsets.UTF_8));
+                this.login = login;
+                System.out.println("User logged in successful!");
+
+                List<ServerWorker> workerList = server.getWorkerList();
+
+                // send current user all other online logins
+                for (ServerWorker worker : workerList) {
+                    if (worker.getLogin() != null) {
+                        if (!login.equals(worker.getLogin())) {
+                            String msg2 = "online " + worker.getLogin() + "\n";
+                            send(msg2);
+                        }
+                    }
+                }
+
+                // send other online users current user's status
+                String onlineMsg = "online " + login + "\n";
+                for (ServerWorker worker : workerList) {
+                    if (!login.equals(worker.getLogin())) {
+                        worker.send(onlineMsg);
+                    }
+                }
+            } else {
+                String msg = "invalid access\n";
+                outputStream.write(msg.getBytes(StandardCharsets.UTF_8));
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private void send(String onlineMsg) throws IOException {
+        if (login != null) {
+            outputStream.write(onlineMsg.getBytes(StandardCharsets.UTF_8));
+        }
+    }
+
+    private boolean checkAccess(String login, String password) {
+        return (login.equals("root") && password.equals("1234")) || (login.equals("ubuntu") && password.equals("ubuntu"));
     }
 }
